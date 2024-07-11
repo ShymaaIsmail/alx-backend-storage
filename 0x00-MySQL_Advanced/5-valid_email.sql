2986dc6f4dc1761741cd85309a9f535c144384e7
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-11 11:45:13 +0300

    task 5

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,13 +3,11 @@
 DELIMITER //
 
 CREATE TRIGGER reset_valid_email
-AFTER UPDATE ON users
+BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
     IF OLD.email <> NEW.email THEN
-        UPDATE users
-        SET valid_email = 0
-        WHERE id = NEW.id;
+        SET NEW.valid_email = 0;
     END IF;
 END;
 //
