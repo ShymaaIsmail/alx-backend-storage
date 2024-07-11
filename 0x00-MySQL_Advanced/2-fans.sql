7be15d90e38ac594a31cd3ae4a47b66655af442e
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-11 09:53:20 +0300

    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Count Non unique fans per country
 -- Order Results by number of fans
-SELECT origin AS origin , COUNT(fans) AS nb_fans
+SELECT origin AS `origin` , COUNT(fans) AS `nb_fans`
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
