3857eefcdd54b960972aa83e2973ab43df5d4db8
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-14 11:04:51 +0300

    task 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -9,8 +9,12 @@ BEGIN
     INSERT INTO projects (name) VALUES (project_name);
     SET project_id = LAST_INSERT_ID();
     END IF;
-
+    
+    SET user_id = (SELECT id FROM users WHERE id = user_id);
+    IF user_id IS NOT NULL
+    THEN
     INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    END IF;
 END;
 //
 
