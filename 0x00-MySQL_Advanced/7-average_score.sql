7fa29c76876317e6b76218556eb25ee863ec84e0
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-14 11:28:54 +0300

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,10 +3,10 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE average_score FLOAT;
+    DECLARE average_score  DECIMAL(5,2);
     DECLARE total_score INT;
     DECLARE corrections_count INT;
-    SELECT SUM(score), COUNT(user_id) INTO total_score, corrections_count FROM corrections WHERE user_id = user_id;
+    SELECT SUM(score), COUNT(*) INTO total_score, corrections_count FROM corrections WHERE user_id = user_id;
     SET average_score = total_score / corrections_count;
     UPDATE users SET average_score = average_score WHERE user_id = user_id;
 END;
