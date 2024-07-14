a14722c94e9c8e9f649a997666068cdc0b0bc5cf
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-14 11:36:40 +0300

    task 7 refix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -6,14 +6,15 @@ BEGIN
     DECLARE average_score  DECIMAL(5,2);
     DECLARE total_score INT;
     DECLARE corrections_count INT;
-    SELECT SUM(score), COUNT(*) INTO total_score, corrections_count FROM corrections WHERE user_id = user_id;
+    SELECT SUM(score), COUNT(*) INTO total_score, corrections_count
+    FROM corrections WHERE corrections.user_id = user_id;
     -- Calculate the average score
     IF corrections_count > 0 THEN
         SET average_score = total_score / corrections_count;
     ELSE
         SET average_score = 0;
     END IF;
-    UPDATE users SET average_score = average_score WHERE id = user_id;
+    UPDATE users SET average_score = average_score WHERE users.id = user_id;
 END;
 //
 DELIMITER ;
