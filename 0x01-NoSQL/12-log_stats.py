7937da0301020687cd407d05a8b932406173815d
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-16 12:15:04 +0300

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Provide some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    ngnx = client.logs.nginx
+    spc = " " * 4
+    print(f"{ngnx.count_documents({})} logs")
+    print("Methods:")
+    print(f"{spc}method GET: {ngnx.count_documents({'method': 'GET'})}")
+    print(f"{spc}method POST: {ngnx.count_documents({'method': 'POST'})}")
+    print(f"{spc}method PUT: {ngnx.count_documents({'method': 'PUT'})}")
+    print(f"{spc}method PATCH: {ngnx.count_documents({'method': 'PATCH'})}")
+    print(f"{spc}method DELETE: {ngnx.count_documents({'method': 'DELETE'})}")
+    print(f"{ngnx.count_documents({'path': '/status'})} status check")
