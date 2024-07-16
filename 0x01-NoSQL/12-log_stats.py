cfd7714bbb96264374f17015cb67069f8e2095be
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-16 12:28:34 +0300

    fix 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,13 +6,12 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     ngnx = client.logs.nginx
-    spc = " " * 4
     print(f"{ngnx.count_documents({})} logs")
     print("Methods:")
-    print(f"{spc}method GET: {ngnx.count_documents({'method': 'GET'})}")
-    print(f"{spc}method POST: {ngnx.count_documents({'method': 'POST'})}")
-    print(f"{spc}method PUT: {ngnx.count_documents({'method': 'PUT'})}")
-    print(f"{spc}method PATCH: {ngnx.count_documents({'method': 'PATCH'})}")
-    print(f"{spc}method DELETE: {ngnx.count_documents({'method': 'DELETE'})}")
+    print(f"\tmethod GET: {ngnx.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {ngnx.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {ngnx.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {ngnx.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {ngnx.count_documents({'method': 'DELETE'})}")
     count = ngnx.count_documents({'path': '/status', 'method': 'GET'})
     print(f"{count} status check")
