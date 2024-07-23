664e6bf80c4489acba6973cd91ffd6c6201bb6f0
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-23 09:16:37 +0300

    task 1

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Exercise """
+
+
+import redis
+from typing import AnyStr, Union
+import uuid
+
+
+class Cache:
+    """Cache Class"""
+    def __init__(self):
+        """Cache init"""
+        self._redis = redis.Redis()
+
+    def __del__(self):
+        """Cache delete"""
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> AnyStr:
+        """Store data"""
+        random_id = str(uuid.uuid1())
+        self._redis.set(random_id, data)
+        return random_id
