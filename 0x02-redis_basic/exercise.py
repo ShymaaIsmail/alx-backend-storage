f6a9e51eafbb6c06663183c1c2edc50b6e62b85c
shymaaismail <shymaa.m.ismail@gmail.com>
2024-07-23 12:29:30 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -64,11 +64,11 @@ class Cache:
         return self.get(key, lambda value: int(value.decode('utf-8')))
 
 def replay(self, method: Callable):
-        """replay to show call history of a function"""
-        inputs = self._redis.lrange(f"{method.__qualname__}:inputs", 0, -1)
-        outputs = self._redis.lrange(f"{method.__qualname__}:outputs", 0, -1)
-        zipped_list = zip(inputs, outputs)
-        name = method.__qualname__
-        print(f"{method.__qualname__} was called {len(zipped_list)} times:")
-        for (i, o) in enumerate(zipped_list):
-            print(f"{name}(*({i.decode('UTF-8')},)) -> {o.decode('UTF-8')}")
+    """replay to show call history of a function"""
+    inputs = self._redis.lrange(f"{method.__qualname__}:inputs", 0, -1)
+    outputs = self._redis.lrange(f"{method.__qualname__}:outputs", 0, -1)
+    zipped_list = zip(inputs, outputs)
+    name = method.__qualname__
+    print(f"{method.__qualname__} was called {len(zipped_list)} times:")
+    for (i, o) in enumerate(zipped_list):
+        print(f"{name}(*({i.decode('UTF-8')},)) -> {o.decode('UTF-8')}")
